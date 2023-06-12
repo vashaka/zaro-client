6a24817754235e29ea6d0a1b50b09da43acdcc3f
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2023-06-12 22:36:05 +0400

    resp design added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,28 +15,6 @@ export default function Home() {
     password: "",
   });
 
-  // useEffect(() => {
-  //   axios.get("https://zaro-api.vercel.app").then((resp) => console.log(resp));
-  // }, []);
-
-  // let redirectUrl: any = "https://facebook.com";
-  // const register = async (e: any) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     await axios.post("https://zaro-api.vercel.app/api/register", {
-  //       email: zaroMail,
-  //       password: zaroPassword,
-  //     });
-
-  //     window.location = redirectUrl;
-
-  //     // setRedirect(true);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
   const handleSignup = async (e: any) => {
     e.preventDefault();
 
@@ -55,10 +33,6 @@ export default function Home() {
         onSubmit={handleSignup}
         className="flex justify-center items-center h-[90vh] flex-col"
       >
-        {/* <div className=" w-1/2 h-[200%] absolute right-0 top-0 bottom-0">
-        <img src="https://onlineschool.emis.ge/assets/svg_icons/art-board.svg#Layer_1%22" />
-      </div> */}
-
         <img
           src="https://onlineschool.emis.ge/assets/svg_icons/logo.svg#Layer_1"
           width={72}
@@ -68,7 +42,7 @@ export default function Home() {
         </h1>
 
         <input
-          className="inp"
+          className="inp w-[80%] md:w-[25rem]"
           type="email"
           placeholder="Enter email"
           required
@@ -82,7 +56,7 @@ export default function Home() {
         />
 
         <input
-          className="inp"
+          className="inp w-[80%] md:w-[25rem]"
           type="password"
           placeholder="Password"
           required
@@ -94,7 +68,7 @@ export default function Home() {
           }
           value={data.password}
         />
-        <button type="submit" className="btn">
+        <button type="submit" className="btn w-[80%] md:w-[25rem]">
           შესვლა
         </button>
 
@@ -107,7 +81,7 @@ export default function Home() {
       </form>
 
       <hr className="h-[1px] border-0 bg-[#3c4eb1]" />
-      <div className="flex justify-between items-center h-[9vh] mx-4 md:mx-12">
+      <div className="flex justify-between items-center h-[9vh] mx-2 md:mx-12">
         <div className="flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -123,7 +97,9 @@ export default function Home() {
               d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
             />
           </svg>
-          <p className="ml-2 text-[15px] text-white">(995 32) 2 200 220</p>
+          <p className="ml-2 text-[15px] text-white whitespace-nowrap">
+            (995 32) 2 200 220
+          </p>
         </div>
         <div className="flex items-center">
           <svg
@@ -140,32 +116,10 @@ export default function Home() {
               d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
             />
           </svg>
-          <p className="ml-2 text-[15px] text-white">onlineschool@emis.ge</p>
+          <p className="ml-2 text-[15px] text-white whitespace-nowrap">
+            onlineschool@emis.ge
+          </p>
         </div>
-
-        {/* <form onSubmit={handleSignup}>
-          <div className="mb-3" controlId="formBasicEmail">
-            <label>Email address</label>
-            <input
-              type="email"
-              placeholder="Enter email"
-              required
-              onChange={(e: any) =>
-                setData({
-                  ...data,
-                  email: e.target.value,
-                })
-              }
-              value={data.email}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label>Password</label>
-          </div>
-
-          <button type="submit">Signup</button>
-        </form> */}
       </div>
     </>
   );
